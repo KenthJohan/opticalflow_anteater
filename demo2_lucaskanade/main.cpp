a640f0f4565ddfdd99321c555ca131299de51c0d
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2023-01-19 17:17:55 +0100

    update

--- demo2_lucaskanade/main.cpp
+++ demo2_lucaskanade/main.cpp
@@ -9,6 +9,9 @@ using namespace cv;
 using namespace std;
 
 #define TRACKER_COUNT 100
+#define CAM_WIDTH 1280
+#define CAM_HEIGHT 720
+#define MOTEST_COUNT 3
 
 void show_trackers(Mat m, vector<Point2f> p0, vector<Scalar> colors)
 {
@@ -19,12 +22,13 @@ void show_trackers(Mat m, vector<Point2f> p0, vector<Scalar> colors)
     }
 }
 
-void show_traces(Mat m, vector<Point2f> p0, vector<Point2f> dir, vector<Scalar> colors)
+void show_traces(Mat m, vector<Point2f> p0, vector<Point2f> dir, vector<Scalar> colors, float gain)
 {
     for(uint i = 0; i < p0.size(); i++)
     {
-        Point2f p1 = p0[i] + dir[i];
-        line(m, p0[i], p1, colors[i], 2);
+        Point2f p1 = p0[i] + dir[i] * gain;
+        //line(m, p0[i], p1, colors[i], 2);
+        arrowedLine(m, p0[i], p1, colors[i], 2);
     }
 }
 
@@ -57,6 +61,7 @@ void motionest_init(motionest_state_t &state)
 
 void motionest_progress(motionest_state_t &state, InputArray f1, InputArray f2)
 {
+    if(state.p0.size() > 0)
     {
         //cv::InputArray prevImg
         //cv::InputArray nextImg
@@ -64,27 +69,54 @@ void motionest_progress(motionest_state_t &state, InputArray f1, InputArray f2)
         //cv::InputOutputArray nextPts
         //cv::OutputArray status
         //cv::OutputArray err
-        cv::Size winSize = cv::Size(21, 21);
+        cv::Size winSize = cv::Size(27, 27);
         int maxLevel = 3;
-        TermCriteria criteria = TermCriteria((TermCriteria::COUNT) + (TermCriteria::EPS), 100, 10.0);
+        TermCriteria criteria = TermCriteria((TermCriteria::COUNT) + (TermCriteria::EPS), 1000, 10.0);
         int flags = 0;
-        double minEigThreshold = 0.0001000000000000000048;
+        double minEigThreshold = 0.000001;
         assert(state.p0.size() > 0);
+        //printf("p0:%i\n", state.p0.size());
         calcOpticalFlowPyrLK(f1, f2, state.p0, state.p1, state.status, state.err, winSize, maxLevel, criteria, flags, minEigThreshold);
+        //printf("p1:%i\n", state.p1.size());
     }
+
+    
 
     state.good_new.clear();
     state.good_new_dir.clear();
-    for(uint i = 0; i < state.p0.size(); i++)
+    //printf("p1:%i\n", state.p1.size());
+    for(uint i = 0; i < state.p1.size(); i++)
     {
         // Select good points
         if(state.status[i] != 1) {continue;}
-        state.good_new.push_back(state.p1[i]);
         Point2f d = state.p1[i] - state.p0[i];
+        if(d.dot(d) > 20.0f){continue;}
+        state.good_new.push_back(state.p1[i]);
         state.good_new_dir.push_back(d);
     }
+    //printf("good_new:%i\n", state.good_new.size());
 
     state.p0 = state.good_new;
+
+    {
+        int n = TRACKER_COUNT - state.p0.size();
+        //printf("TRACKER_COUNT:%i, p0:%i, n:%i\n", TRACKER_COUNT, state.p0.size(), n);
+        vector<Point2f> fresh;
+        if (n > 0)
+        {
+            goodFeaturesToTrack(f1, fresh, n, 0.1, 13, Mat(), 3, false, 0.04);
+            //printf("fresh:%i, n:%i\n", fresh.size(), n);
+            for(uint i = 0; i < fresh.size(); i++)
+            {
+                state.p0.push_back(fresh[i]);
+            }
+            //printf("p0:%i\n", state.p0.size());
+        }
+
+    }
+
+    //printf("\n");
+
 }
 
 
@@ -95,7 +127,7 @@ void motionest_progress(motionest_state_t &state, InputArray f1, InputArray f2)
 
 void motionest_progress_reset(motionest_state_t &state, InputArray f1)
 {
-    goodFeaturesToTrack(f1, state.p0, TRACKER_COUNT, 0.1, 13, Mat(), 3, false, 0.04);
+    //goodFeaturesToTrack(f1, state.p0, TRACKER_COUNT, 0.1, 13, Mat(), 3, false, 0.04);
 }
 
 
@@ -126,6 +158,9 @@ int main(int argc, char **argv)
     }
     VideoCapture capture(filename);
     capture.set(CAP_PROP_POS_FRAMES, 250);
+    printf("CAP_PROP_FRAME_WIDTH  : %i\n", (int)capture.get(CAP_PROP_FRAME_WIDTH));
+    printf("CAP_PROP_FRAME_HEIGHT : %i\n", (int)capture.get(CAP_PROP_FRAME_HEIGHT));
+
     if (!capture.isOpened()){
         //error in opening the video input
         cerr << "Unable to open file!" << endl;
@@ -134,8 +169,18 @@ int main(int argc, char **argv)
     // Create some random colors
 
 
-    motionest_state_t mot1;
-    motionest_init(mot1);
+
+    motionest_state_t motest[MOTEST_COUNT];
+    Rect roi[MOTEST_COUNT];
+    roi[0] = Rect(400, 50, 500, CAM_HEIGHT-250);
+    roi[1] = Rect(1400, 50, 400, CAM_HEIGHT-150);
+    roi[2] = Rect(1810, 50, 400, CAM_HEIGHT-200);
+
+    for(int i = 0; i < MOTEST_COUNT; ++i)
+    {
+        motionest_init(motest[i]);
+    }
+    
 
 
     Mat frame;
@@ -160,7 +205,7 @@ int main(int argc, char **argv)
 
 
         capture >> frame;
-        printf("CAP_PROP_POS_FRAMES %i!\n", (int)capture.get(CAP_PROP_POS_FRAMES));
+        //printf("CAP_PROP_POS_FRAMES %i!\n", (int)capture.get(CAP_PROP_POS_FRAMES));
         if (frame.empty())
         {
             printf("Video ended!\n");
@@ -174,22 +219,34 @@ int main(int argc, char **argv)
 
 
 
-        if((frame_index % 50) == 0)
+
+        /*
+        if((frame_index % 10) == 0)
         {
-            motionest_progress_reset(mot1, f2);
-            //mask = Mat::zeros(frame.size(), frame.type());
+            for(int i = 0; i < MOTEST_COUNT; ++i)
+            {
+                motionest_progress_reset(motest[i], f2(roi[i]));
+            }
+            mask = Mat::zeros(frame.size(), frame.type());
         }
+        */
 
-
-        if(mot1.p0.size() > 0)
+        for(int i = 0; i < MOTEST_COUNT; ++i)
         {
             // calculate optical flow
-            motionest_progress(mot1, f1, f2);
+            motionest_progress(motest[i], f1(roi[i]), f2(roi[i]));
         }
- 
 
-        show_trackers(frame, mot1.good_new, mot1.colors);
-        //show_traces(mask, mot1.good_new, mot1.good_new_dir, mot1.colors);
+
+
+        for(int i = 0; i < MOTEST_COUNT; ++i)
+        {
+            show_trackers(frame(roi[i]), motest[i].good_new, motest[i].colors);
+            show_traces(frame(roi[i]), motest[i].good_new, motest[i].good_new_dir, motest[i].colors, 10.0f);
+            rectangle(frame, roi[i], Scalar(0, 0, 255), 4);
+        }
+
+
         Mat img;
         add(frame, mask, img);
         imshow("Frame", img);
